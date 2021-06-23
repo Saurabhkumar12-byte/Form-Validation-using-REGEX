d4891c22d041bd4978a4d4de9f7dab54b31ac8d5
Saurabh <saurabhsingh4778698@gmail.com>
2021-06-23 20:40:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const Username= document.getElementById("Username");
+const email= document.getElementById("email");
+const phone= document.getElementById("phone");
+
+Username.addEventListener('blur',()=>{
+    console.log('blurred');
+    
+})
+email.addEventListener('blur',()=>{
+    console.log('blurred');
+    
+})
+phone.addEventListener('blur',()=>{
+    console.log('blurred');
+    
+})
+
